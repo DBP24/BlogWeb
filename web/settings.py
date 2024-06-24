e1dcae7fff6ebadeb75d3f727e4debf65cb0cf66
DBP24 <bonattipajuelodiego2406@gmail.com>
2024-06-24 10:29:49 -0500

    add bookshop of django-material

--- web/settings.py
+++ web/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django Material Dashboard Admin
+    'admin_material.apps.AdminMaterialDashboardConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -129,3 +132,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- web/urls.py
+++ web/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('admin_material.urls')),
     path('admin/', admin.site.urls),
     path('page/', include('app.page.urls')),
 ]
